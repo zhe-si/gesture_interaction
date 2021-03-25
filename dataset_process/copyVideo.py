116adac84e7ca36d88dc498bef234f84ff493ff3
lq <1464445232@qq.com>
2021-03-25 16:33:22 +0800

    完成视频剪辑脚本

---
+++ dataset_process/copyVideo.py
@@ -0,0 +1,56 @@
+# 键盘手势视频，从n个文件夹拷贝中按照时间顺序每个拷贝m个视频到对应文件夹
+import os
+import shutil
+
+
+def main():
+    root_path = ""
+    from_path1 = os.path.join(root_path, "")
+    from_path2 = os.path.join(root_path, "")
+    from_path3 = os.path.join(root_path, "")
+    from_path4 = os.path.join(root_path, "")
+    to_path = os.path.join(root_path, "")
+    make_sure_path_exits(to_path)
+
+    video_list1 = get_file_list(from_path1)
+    video_list2 = get_file_list(from_path2)
+    video_list3 = get_file_list(from_path3)
+    video_list4 = get_file_list(from_path4)
+    for i in range(10 * 25):
+        left_right = "left"
+        if i >= 5 * 25:
+            left_right = "right"
+        finger = i // 25 + 1
+        point = i % 25 + 1
+        dir_name = left_right + "-" + str(finger) + "-" + str(point)
+        dir_name = os.path.join(to_path, dir_name)
+        make_sure_path_exits(dir_name)
+        move_n_file(from_path1, dir_name, video_list1, 2)
+        move_n_file(from_path2, dir_name, video_list2, 2)
+        move_n_file(from_path3, dir_name, video_list3, 2)
+        move_n_file(from_path4, dir_name, video_list4, 2)
+    pass
+
+
+# 从旧到新移动
+def move_n_file(from_path, to_path, video_list, n: int):
+    for i in range(n):
+        file_name = video_list.pop(0)
+        shutil.move(os.path.join(from_path, file_name), os.path.join(to_path, file_name))
+
+
+def make_sure_path_exits(dir_name):
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
+
+
+def get_file_list(path: str):
+    if not os.path.isdir(path):
+        raise Exception("{} is not dir".format(path))
+    file_list = os.listdir(path)
+    file_list = sorted(file_list, key=lambda x: os.path.getmtime(os.path.join(path, x)))
+    return file_list
+
+
+if __name__ == '__main__':
+    main()
---
+++ dataset_process/cutVideo.py
@@ -0,0 +1,118 @@
+# 播放视频根据输入剪切后另存为到标签对应路径
+# 可将完整视频切成单个动作一个视频，并放到对应标签文件夹
+import os
+
+import cv2
+
+# 键盘按键：标签
+sign_map = {"1": "1", "2": "2"}
+
+
+def main():
+    # 保存路径
+    to_path = "E:/to"
+    # 单个视频路径
+    video_path = "E:/WIN_20210322_19_46_16_Pro.mp4"
+    # 视频文件夹路径
+    videos_dir_path = "E:/k"
+
+    show_interval = 0.3
+    fourcc = cv2.VideoWriter_fourcc('D', 'I', 'V', 'X')  # mp4编节码器
+    video_type = ".mp4"
+
+    # 剪切单个视频
+    # cut_video(fourcc, show_interval, to_path, video_path, video_type)
+    # 剪切多个视频（videos_dir_path是视频文件夹路径）
+    cut_videos(fourcc, show_interval, to_path, videos_dir_path, video_type)
+    pass
+
+
+# 剪切多个文件，输入一个原视频文件夹
+def cut_videos(fourcc, show_interval, to_path, videos_dir_path, video_type):
+    videos_path = os.listdir(videos_dir_path)
+    for video_path in videos_path:
+        path = os.path.join(videos_dir_path, video_path)
+        if os.path.isfile(path):
+            print("start cut video {}".format(video_path))
+            cut_video(fourcc, show_interval, to_path, path, video_type)
+            print("end cut video {}".format(video_path))
+
+
+# 剪切单个视频
+def cut_video(fourcc, show_interval, to_path, video_path, video_type):
+    cap = cv2.VideoCapture(video_path)
+
+    frame_num = int(cap.get(7))
+    fps = int(cap.get(5))  # 获得视频帧速率
+    time = frame_num / fps
+    show_frame = int(frame_num / (time / show_interval))
+    width, height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    i = 0
+    video_index = 1
+    sign_state = (" ", " ")
+    video_writer = None
+    while True:
+        success, frame = cap.read()
+        if success:
+            i += 1
+            # 每show_frame显示一次图片并判断是否
+            if i % show_frame == 1:
+                cv2.imshow("show", frame)
+                while True:
+                    key = chr(cv2.waitKey(0))
+                    # 跳过
+                    if key == " ":
+                        if video_writer is not None:
+                            video_writer.write(frame)
+                        break
+                    # 开始某标签剪切
+                    elif key == "s":
+                        if sign_state[0] != " ":
+                            print("last sign is not end")
+                            continue
+                        key2 = chr(cv2.waitKey(0))
+                        if key2 not in sign_map:
+                            print("don't have this sign, start sign false")
+                        else:
+                            to_video_path = os.path.join(to_path, sign_map[key2])
+                            mack_sure_dir_exit(to_video_path)
+                            video_writer = \
+                                cv2.VideoWriter(os.path.join(to_video_path, str(video_index) + video_type),
+                                                fourcc, fps, (width, height))
+                            video_writer.write(frame)
+                            sign_state = ("s", key2)
+                            video_index += 1
+                            print("start sign {}".format(sign_map[key2]))
+                        continue
+                    # 结束某标签剪切
+                    elif key == "e":
+                        if sign_state[0] != "s":
+                            print("sign is not start")
+                            continue
+                        key2 = chr(cv2.waitKey(0))
+                        if key2 != sign_state[1]:
+                            print("end sign {} is not match start sign {}, end sign false"
+                                  .format(key2, sign_map[sign_state[1]]))
+                        else:
+                            video_writer.write(frame)
+                            video_writer.release()
+                            video_writer = None
+                            sign_state = (" ", " ")
+                            print("end sign {}".format(sign_map[key2]))
+                        continue
+            else:
+                if video_writer is not None:
+                    video_writer.write(frame)
+        else:
+            print('end video {}'.format(video_path))
+            break
+
+
+def mack_sure_dir_exit(to_video_path):
+    if not os.path.exists(to_video_path):
+        os.makedirs(to_video_path)
+
+
+if __name__ == '__main__':
+    main()
